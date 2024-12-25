7fc2353fca210971744f80263c4e78e1452a438d
dongxuan li <dongxuanlizju@gmail.com>
2024-12-25 15:57:26 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -140,11 +140,11 @@ def get_daily_papers(topic,query="slam", max_results=2):
                 content_to_web[paper_key] = "- {}, **{}**, {} et.al., Paper: [{}]({}), Code: **[{}]({})**".format(
                        update_time,paper_title,paper_first_author,paper_url,paper_url,repo_url,repo_url)
 
-            else:
-                content[paper_key] = "|**{}**|**{}**|{} et.al.|[{}]({})|null|\n".format(
-                       update_time,paper_title,paper_first_author,paper_key,paper_url)
-                content_to_web[paper_key] = "- {}, **{}**, {} et.al., Paper: [{}]({})".format(
-                       update_time,paper_title,paper_first_author,paper_url,paper_url)
+            #else:
+            #    content[paper_key] = "|**{}**|**{}**|{} et.al.|[{}]({})|null|\n".format(
+            #           update_time,paper_title,paper_first_author,paper_key,paper_url)
+            #    content_to_web[paper_key] = "- {}, **{}**, {} et.al., Paper: [{}]({})".format(
+            #           update_time,paper_title,paper_first_author,paper_url,paper_url)
 
             # TODO: select useful comments
             comments = None
